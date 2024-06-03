6f6da3657919e4695e45511ef008d960e1d75def
Anirvan Bhaduri <anirvan.bhaduri@gmail.com>
2024-06-04 01:50:10 +0200

    Moved game to playground.

---
+++ app/playground/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useState } from 'react';
+import GamePanel from './components/GamePanel';
+import LogPanel from './components/LogPanel';
+import { GameLogContext, LogMsg } from './game/game-logger';
+import HelpText from './components/HelpText';
+
+export default function PlayGround() {
+  // NOTE: using logger with a LoggerContext for fun
+  // to try out contexts. Otherwise, we could have simply
+  // passed the logger through as a prop!
+  const [gameLogs, setGameLogs] = useState<LogMsg[]>([]);
+
+  return (
+    <main className="z-[-1] flex grow flex-col gap-5 py-4 px-4 md:px-10 lg:px-24">
+      <h1 className="text-2xl font-bold text-center my-5">Ping pong game</h1>
+      <div className="flex flex-col lg:flex-row gap-5">
+        <GameLogContext.Provider value={{ gameLogs, setGameLogs: (log) => setGameLogs((logs) => [...logs, log]) }}>
+          <div className="w-full lg:w-2/3 flex-auto text-center">
+            <GamePanel />
+          </div>
+          <div className="w-full lg:w-1/3 flex-auto text-center">
+            <LogPanel />
+          </div>
+        </GameLogContext.Provider>
+      </div>
+      <HelpText />
+    </main>
+  );
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home: React.FunctionComponent = () => {
+  return (
+    <main className="h-full w-full px-4 md:px-10 lg:px-24 flex grow items-center">
+      <h1 className="text-6xl text-zinc-100 text-center">
+        Welcome to <span className="text-sky-600">Anirvan Bhaduri's</span> website!
+      </h1>
+    </main>
+  );
+};
+
+export default Home;
