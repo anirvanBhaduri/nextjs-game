36c982c1fcc901eaf0efb7f2a1e67e305dacc123
Anirvan Bhaduri <anirvan.bhaduri@gmail.com>
2024-06-04 01:50:28 +0200

    Added home page with simple landing text.

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,13 @@
-'use client';
+import React from 'react';
 
-import { useState } from 'react';
-import GamePanel from './components/GamePanel';
-import LogPanel from './components/LogPanel';
-import { GameLogContext, LogMsg } from './game/game-logger';
-import HelpText from './components/HelpText';
-
-export default function Home() {
-  // NOTE: using logger with a LoggerContext for fun
-  // to try out contexts. Otherwise, we could have simply
-  // passed the logger through as a prop!
-  const [gameLogs, setGameLogs] = useState<LogMsg[]>([]);
-
+const Home: React.FunctionComponent = () => {
   return (
-    <main className="flex flex-col gap-5">
-      <div className="flex flex-col lg:flex-row lg:flex-1 gap-5">
-        <GameLogContext.Provider value={{ gameLogs, setGameLogs: (log) => setGameLogs((logs) => [...logs, log]) }}>
-          <div className="w-full lg:w-2/3 flex-auto text-center">
-            <GamePanel />
-          </div>
-          <div className="w-full lg:w-1/3 flex-auto text-center">
-            <LogPanel />
-          </div>
-        </GameLogContext.Provider>
-      </div>
-      <HelpText />
+    <main className="h-full w-full px-4 md:px-10 lg:px-24 flex grow items-center">
+      <h1 className="text-6xl text-zinc-100 text-center">
+        Welcome to <span className="text-sky-600">Anirvan Bhaduri's</span> website!
+      </h1>
     </main>
   );
-}
+};
+
+export default Home;
--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,8 +3,8 @@ import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 const Footer: React.FunctionComponent = () => {
   return (
-    <footer className="w-full relative h-10">
-      <section className="text-md text-zinc-200 bottom-0 text-right align-center h-10 fixed w-full px-4 flex gap-4 flex-row-reverse">
+    <footer className="w-full relative h-20">
+      <section className="text-md text-zinc-200 bg-gradient-to-t dark:from-black dark:via-black bottom-0 text-right items-end h-20 fixed w-full px-4 pb-4 flex gap-4 flex-row-reverse">
         <a target="_blank" href="https://github.com/anirvanBhaduri">
           <FaGithub className="text-2xl"></FaGithub>
         </a>
