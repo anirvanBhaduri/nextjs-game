17f8d5f13703f527032d54eb75fa854bcbe7e00e
Anirvan Bhaduri <anirvan.bhaduri@gmail.com>
2024-06-04 20:04:42 +0200

    Reformatted home page and included profile pic.

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Home: React.FunctionComponent = () => {
   return (
-    <main className="h-full w-full px-4 md:px-10 lg:px-24 flex grow items-center">
-      <h1 className="text-6xl text-zinc-100 text-center">
-        Welcome to <span className="text-sky-600">Anirvan Bhaduri's</span> website!
+    <main className="h-full w-full px-4 md:px-10 lg:px-24 flex flex-col grow items-center">
+      <section className="profile-image bg-top h-40 w-40 md:h-64 md:w-64 lg:h-64 lg:w-64 bg-cover rounded-full my-10"></section>
+      <h1 className="text-5xl text-zinc-100 text-center">
+        Welcome to <span className="text-sky-400">Anirvan Bhaduri's</span> website!
       </h1>
     </main>
   );
